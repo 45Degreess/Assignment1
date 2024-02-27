69b449e5c5c0489394e44ba699462d23ac28090c
Chidiebere Umah <chidie.umah@gmail.com>
2024-02-27 13:55:08 +0200

    Added functionality to insert data from text file - GenericsKB_BST

--- src/GenericsKB_BST.java
+++ src/GenericsKB_BST.java
@@ -1,3 +1,35 @@
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 public class GenericsKB_BST
 {
+    private BinarySearchTree<Generic> bst;
+
+    public GenericsKB_BST(String file)
+    {
+        try
+        {
+            BufferedReader ff= new BufferedReader(new FileReader(file));
+            String line;
+            while((line = ff.readLine()) != null)
+            {
+                String temp []= line.split("\\t");
+                String term = temp[0];
+                String sentence = temp[1];
+                double confidence = Double.parseDouble(temp[2]);
+                bst.insert(new Generic(term, sentence, confidence));
+            }
+            ff.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
