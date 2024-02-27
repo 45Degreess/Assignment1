ff65330a0c689df5a885e90c86266cb3ee1ea05a
Chidiebere Umah <chidie.umah@gmail.com>
2024-02-27 10:17:48 +0200

    Created a BinarySearchTree node object - BSTNode

---
+++ src/BSTNode.java
@@ -0,0 +1,23 @@
+public class BSTNode<dataType>
+{
+    private dataType data;
+    private BSTNode<dataType> left;
+    private BSTNode<dataType> right;
+
+    public BSTNode(dataType data,BSTNode left,BSTNode right)
+    {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public BSTNode<dataType> getLeft()
+    {
+        return left;
+    }
+
+    public BSTNode<dataType> getRight()
+    {
+        return right;
+    }
+}
